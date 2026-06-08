097fd4c6a534dd3794d332e3803bc806be7659fc
Francisco Fonseca <francisco.fonseca.farias@gmail.com>
2026-06-07 23:00:54 -0600

    mecora con los correo

--- core/settings.py
+++ core/settings.py
@@ -143,7 +143,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#  correo de Gmail  para el proyecto
+
+
 EMAIL_HOST_USER = 'francisco.fonseca.farias@gmail.com' 
-# OJO: No es tu contraseña normal. Es una "Contraseña de Aplicación" de 16 letras.
-EMAIL_HOST_PASSWORD = 'mtfu ffoh zpdz djpa'
+
+
+EMAIL_HOST_PASSWORD = 'mtfuffohzpdzdjpa'
+
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
