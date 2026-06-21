58759831bfcc8793cc59927374c4397b49ea42bf
Francisco Fonseca <francisco.fonseca.farias@gmail.com>
2026-06-21 16:14:54 -0600

    Configurado modelo de flota 3

--- flota/admin.py
+++ flota/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Ruta, Unidad
+from .models import HistorialTurno, MensajeFlota, RegistroSesion, Ruta, Unidad
+from rutas.models import Ruta
 
 admin.site.register(Ruta)
 admin.site.register(Unidad)
+admin.site.register(Unidad)
+admin.site.register(MensajeFlota)
+admin.site.register(RegistroSesion)
+admin.site.register(HistorialTurno)
--- flota/models.py
+++ flota/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from rutas.models import Ruta
 class Unidad(models.Model):
     ESTADOS = (
         ('operativa', 'Operativa (En Ruta)'),
