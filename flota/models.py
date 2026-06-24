e2f50e25105aabc8c949e42eaf2647a9829eef00
Francisco Fonseca <francisco.fonseca.farias@gmail.com>
2026-06-24 00:15:34 -0600

    Configurado motor telegran 2

--- flota/models.py
+++ flota/models.py
@@ -121,22 +121,7 @@ class ControlLegal(models.Model):
     def __str__(self):
         return f"Legal - {self.unidad.numero_unidad}"
 
-class ControlLegal(models.Model):
-    """Guarda las fechas de vencimiento para el INTT y la Alcaldía de Caroní"""
-    unidad = models.OneToOneField(Unidad, on_delete=models.CASCADE, related_name='legal')
-    carnet_circulacion = models.BooleanField(default=False)
-    vencimiento_rcv = models.DateField(null=True, blank=True)
-    vencimiento_imttv = models.DateField(null=True, blank=True)
-    pago_alcaldia = models.CharField(max_length=50, default='Sin registrar', choices=[
-        ('Al día', 'Al día'),
-        ('Pendiente T1', 'Pendiente T1'),
-        ('Pendiente T2', 'Pendiente T2'),
-        ('Pendiente T3', 'Pendiente T3'),
-        ('En Mora', 'En Mora')
-    ])
-    
-    def __str__(self):
-        return f"Legal - {self.unidad.numero_unidad}"   
+  
     
 # =========================================================
 # MÓDULO DE ALERTAS TELEGRAM (Señal Ética +)
